fb682fdcd5e7c8980208e80b0d5f6c7a3e97c416
+ <+>
2022-08-18 20:54:43 +0300

    Complete Assignments

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+ const num1= 31;
+ const num2=2;
+
+var multiply = num1*num2;
+
+var random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 53;
+const num4 = 7;
+var mod = num3 % num4;
+
+var max = Math.max(1, 2, 3, 20, 5);
